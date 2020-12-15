e7073daa2687d8e8e76d1f00d63e7a56717cd3dd
Raymart D. Baradas <rdbaradas@multisyscorp.com>
2020-12-16 03:10:16 +0800

    url update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
-        $this->call(CategorySeeder::class);
+        // $this->call(CategorySeeder::class);
         // $this->call(LessonsSeeder::class);
     }
 }
