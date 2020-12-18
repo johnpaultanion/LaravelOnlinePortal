cbbb71bfc3a14b29232fc2fd30594f3923909f60
Raymart D. Baradas <rdbaradas@multisyscorp.com>
2020-12-18 20:30:31 +0800

    fix url bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultstringLength(191);
-
         if (config('app.env') !== 'local') {
             URL::forceScheme('https');
         }
