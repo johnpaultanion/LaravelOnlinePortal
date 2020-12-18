48983c899a4837f0d74c6f9ba3683942c1dfc812
Raymart D. Baradas <rdbaradas@multisyscorp.com>
2020-12-18 20:07:56 +0800

    fix url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,12 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultstringLength(191);
 
-        env('APP_ENV') == 'local' ? URL::forceScheme('http') : URL::forceScheme('https');
+        // env('APP_ENV') == 'local' ? URL::forceScheme('http') : URL::forceScheme('https');
+
+        if(env('APP_ENV') == 'local'){
+            URL::forceScheme('http');
+        }else{
+            URL::forceScheme('https');
+        }
     }
 }
